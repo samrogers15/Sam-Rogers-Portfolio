2086afe7b38105e4627bf7ae76d9cc65f2986d36
Sam Rogers <samrogers15@gmail.com>
2021-06-09 21:13:15 -0700

    updated projects with links to respositories

--- src/Components/Pages/Work/Work.js
+++ src/Components/Pages/Work/Work.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import WorkCard from "./WorkCard";
 import JumboTron from "../../Jumbotron/JumboTron";
 import "./Work.css";
-import burger from "../../../Assets/Projects/eat-da-burgers.png";
 import employee from "../../../Assets/Projects/employeedirectory.png";
 import recipe from "../../../Assets/Projects/recipeappexample.png";
 import planner from "../../../Assets/Projects/Interactive-Entertainment-Planner.png";
@@ -27,7 +26,9 @@ const Work = () => {
               imgPath={showmyshow}
               title="ShowMyShow"
               description="ShowMyShow is an application that allows fans and bands to connect with one another. Users (fans or bands) can sign up using a valid email address and connect to a one-stop shop that allows them to discover  tons of information on upcoming music events in a given city. Fans can search for concerts in a specific city and are returned a list of upcoming shows with specific information and links to tickets via Ticketmaster - fans can also save shows to their profile and come back to them at a later date. There is a messaging function - the \'Green Room\' - where fans can connect with other fans to talk about music. Additionally, upon login, fans and bands alike are provided with a scrape of Tweets from the hashtag #ShowMyShow directly on their login page so they can keep current with any immediate information shared via Twitter."
-              link="www.showmyshow.org"
+              skills="Core project responsibilities included application wireframe and design, authentication and database integration, messaging functionality, Ticketmaster and Twitter API integration, and overall application debugging."
+              link="https://www.showmyshow.org/"
+              github="https://github.com/PuppyRoadTrip/showmyshow"
             />
           </Col>
           <Col lg={6} className="workCol">
@@ -35,7 +36,9 @@ const Work = () => {
               imgPath={recipe}
               title="Everyday Recipes App"
               description="Cooking can be an incredible experience but it can also be incredibly frustrating. Surfing through old cookbooks with stained hard to read recipe cards can be exhausting and take all the joy out of the kitchen. This was the issue posed to me by my partner, Kelsey. She wanted a way to store and access her favorite recipes in a one stop location. Enter Everyday Recipes, an easy to use recipe app that allows a user to add, store, edit, delete, the recipes they love. The app is designed, tried and tested by developers and users alike who are every day at home cooks."
+              skills="Core project responsibilities included application wireframe and design, authentication and database integration, search functionality, routing, and overall application debugging."
               link="https://my-favorite-recipes-app-portla.herokuapp.com/"
+              github="https://github.com/jinxdoll/my-favorite-recipes-app"
             />
           </Col>
           <Col lg={6} className="workCol">
@@ -43,7 +46,9 @@ const Work = () => {
               imgPath={planner}
               title="Interactive Entertainment Planner"
               description="Let’s set the stage - it’s Friday evening. You’re topping off a LONG week of work. The weather is nasty. You’re completely fried. You just NEED. TO. RELAX. But you can’t muster up the effort to figure out how to unwind. WORRY NOT! Hop on over to the Interactive Entertainment Planner. With minimal effort, you’ll be able grab a movie recommendation based on a simple input, generate a random cocktail to make for yourself, and find recipes based on ingredients that you want to include."
+              skills="Core project responsibilities included application wireframe and design, API integration, search functionality, design, and overall application debugging."
               link="https://samrogers15.github.io/Interactive_Entertainment_Planner/"
+              github="https://github.com/samrogers15/Interactive_Entertainment_Planner"
             />
           </Col>
           <Col lg={6} className="workCol">
@@ -51,7 +56,9 @@ const Work = () => {
               imgPath={employee}
               title="MySQL Employee Tracker"
               description="This is a command line application that allows a user to manage information on employees within a company. The application connects to a database housed in MySQL that contains three tables with information on departments, roles, and employees within the company. This Content Management System allows a user to add, view, and modify information about employees of a company."
+              skills="Core project responsibilities include JavaScript, Node, NPM Inquirer, NPM MySQL, NPM console.table, and MySQL Workbench."
               link="https://drive.google.com/file/d/1_20AT6cerIgXbkMycODh86IQtiO5k_qQ/view"
+              github="https://github.com/samrogers15/MySQL-employee-tracker"
             />
           </Col>
         </Row>
--- src/Components/Pages/Work/WorkCard.js
+++ src/Components/Pages/Work/WorkCard.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const WorkCard = ({ imgPath, title, description, link }) => {
+const WorkCard = ({ imgPath, title, description, skills, link, github }) => {
   return (
     <Card className="workCard">
       <Card.Img className="workCardImg" src={imgPath} alt="workImg" />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary" href={link} target="_blank">
-          <i className="cil-external-link">&nbsp;</i> View Project
+        <Card.Text>{skills}</Card.Text>
+        <Button variant="primary" href={link} target="_blank" id="link">
+          <i className="cil-external-link">&nbsp;</i>View Project
+        </Button>
+        <Button variant="primary" href={github} target="_blank">
+          <i className="cil-external-link">&nbsp;</i>View GitHub Repo
         </Button>
       </Card.Body>
     </Card>
